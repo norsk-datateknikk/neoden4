ca47c32183b701b09a6a97aaf6a895f5c997a430
Erik Buer <erik.buer@norskdatateknikk.no>
2021-10-04 14:51:56 +0200

    Added license clause in ND-written file.

--- csv2uss.py
+++ csv2uss.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+#--------------------------------------------------------------#
+# Copyright 2021 Norsk Datateknikk AS                          #
+#--------------------------------------------------------------#
+# This file is subject to the terms and conditions defined in  #
+# file 'LICENSE', which is part of this source code package.   #
+#--------------------------------------------------------------#
+
 import sys
 import os
 import pandas as pd
